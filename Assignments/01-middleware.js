942650c1b9a33335e4a112b70f8e682a6e5d3bce
Umang Agarwal <umangagarwal257@gmail.com>
2025-01-29 18:12:28 +0530

    Cleaner Code

--- Assignments/01-middleware.js
+++ Assignments/01-middleware.js
@@ -3,13 +3,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
+const loggerMiddleware = (req, res, next) => {
   const timestamp = new Date().toUTCString();
-  console.log(
-    `Time : [${timestamp}] , Method : ${req.method}, URL : ${req.url}`
-  );
+  console.log(`Time : [${timestamp}] `);
+  console.log(`Method : ${req.method}`);
+  console.log(`URL : ${req.url}`);
   next();
-});
+};
+
+app.use(loggerMiddleware);
 
 app.get("/user", (req, res) => {
   res.json({ msg: "Hello, World!" });

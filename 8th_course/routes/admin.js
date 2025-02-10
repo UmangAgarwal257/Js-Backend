5e6f8db6c1649d9680d4557212bea7219fc442df
Umang Agarwal <umangagarwal257@gmail.com>
2025-02-10 20:00:39 +0530

    added routes and route skeletons

---
+++ 8th_course/routes/admin.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+
+const adminRouter = Router();
+
+adminRouter.use(adminMiddleware);
+
+adminRouter.post("/signup", (req, res) => {
+  res.json({
+    message: "signup endpoint",
+  });
+});
+
+adminRouter.post("/signin", (req, res) => {
+  res.json({
+    message: "signin endpoint",
+  });
+});
+
+adminRouter.post("/course", (req, res) => {
+  res.json({
+    message: "course endpoint",
+  });
+});
+
+adminRouter.put("/course", (req, res) => {
+  res.json({
+    message: "course endpoint",
+  });
+});
+
+adminRouter.get("/course/all", (req, res) => {
+  res.json({
+    message: "course endpoint",
+  });
+});
+
+module.exports = {
+  adminRouter: adminRouter,
+};
